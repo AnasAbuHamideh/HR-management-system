20ff7c6ab1ec05040482e15078e2649193072559
AnasAbuHamideh <anas.abuhamida@gmail.com>
2022-01-26 19:08:42 +0200

    done

--- JS/app.js
+++ JS/app.js
@@ -8,14 +8,17 @@ function Employee(Employee_ID, Full_Name, Department, Level,imgUrl) {
 Employee.prototype.calcSalary = function () {
     if (this.Level == "Senior"){
         this.Salary = Math.floor(Math.random() * (2000-1500) + 1500);
+        this.Salary=this.Salary-(this.Salary*0.075);
     console.log(this.Salary);
 }
 else if (this.Level == "Mid-Senior"){
         this.Salary = Math.floor(Math.random() * (1500-1000) + 1000);
+        this.Salary=this.Salary-(this.Salary*0.075);
     console.log(this.Salary);
 }
 else if (this.Level == "Junior"){
     this.Salary = Math.floor(Math.random() * (1000-500) + 500);
+    this.Salary=this.Salary-(this.Salary*0.075);
 console.log(this.Salary);
 }
 
@@ -53,3 +56,37 @@ const e7=new Employee(1006,	"Hadi Ahmad","Finance","Mid-Senior");
 e7.calcSalary();
 e7.render();
 
+let EForm = document.getElementById('form1');
+
+
+let addnewemployee=function(event){
+    event.preventDefault();
+    let name= event.target.name.value;
+    let imgUrl = event.target.img.value;
+    let department = event.target.depart.value;
+    let level = event.target.Level.value;
+    let employeeId = Math.floor((Math.random() * (1025 - 1007)) + 1007)
+    console.log(name, imgUrl, department, level, employeeId);
+    const newEmployee = new Employee(employeeId, name, department, level, imgUrl)
+    newEmployee.calcSalary();
+    newEmployee.render2();
+}
+EForm.addEventListener('submit',addnewemployee)
+
+Employee.prototype.render2=function(){
+    let divEl=document.getElementById('div');
+    let imgEl=document.createElement('img');
+    divEl.appendChild(imgEl);
+    imgEl.setAttribute('src', this.imageURL);
+    imgEl.setAttribute('alt', "image");
+    let pEl=document.createElement('p');
+    divEl.appendChild(pEl);
+    // pEl.setAttribute('Name:',this.name);
+    // pEl.setAttribute('ID:',this.Employee_ID);
+    // pEl.setAttribute('Department:',this.Department);
+    // pEl.setAttribute('Level:',this.Level);
+    // pEl.setAttribute('Salary:',this.Salary);
+    pEl.textContent=`Name:${this.Full_Name}-ID:${this.Employee_ID}-Department:${this.Department}-Level:${this.Level}-Salary:${this.Salary}`;
+    let brEl=document.createElement('br');
+    divEl.appendChild(brEl);
+}
